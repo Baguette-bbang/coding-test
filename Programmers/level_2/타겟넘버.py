4275f8cd5b00c60d4c1063880a15db29ce984cb2
Baguette-bbang <dudals9701@naver.com>
2023-12-26 21:04:14 +0900

    Search

--- Programmers/level_2/타겟넘버.py
+++ Programmers/level_2/타겟넘버.py
@@ -1,26 +1,19 @@
 def solution(numbers, target):
     answer = 0
-    # 각 number마다 부호를 추가해서 2차원 행렬로 만듦.
-    graph = [[] for _ in range(len(numbers))]
-    for i in range(len(numbers)):
-        graph[i].append(numbers[i])
-        graph[i].append(numbers[i]*-1)
     
-    # 계산된 값을 저장하기 위한 리스트 초기화
-    queue = []
-    queue.append(graph[0][0])
-    queue.append(graph[0][1])
+    #초기값 설정
+    cal_list = []
+    cal_list.append(numbers[0])
+    cal_list.append(-numbers[0])
     
     # 모든 경우의 수 탐색
     for i in range(1,len(numbers)): 
         temp = []
-        for num in queue: 
-            for z in range(2):
-                temp.append(num+graph[i][z])
-        queue= temp
+        for num in cal_list: 
+            temp.append(num+numbers[i])
+            temp.append(num-numbers[i])
+        cal_list = temp
     
     # 모든 계산된 값들 중 target과 일치하는 값의 개수를 return
-    for i in queue:
-        if i == target:
-            answer +=1
+    answer = cal_list.count(target)
     return answer
