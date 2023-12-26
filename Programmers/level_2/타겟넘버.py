7f462be5c6e9e5c049ceed989c86be90dac1dd11
Baguette-bbang <dudals9701@naver.com>
2023-12-26 19:29:09 +0900

    DFS

--- Programmers/level_2/타겟넘버.py
+++ Programmers/level_2/타겟넘버.py
@@ -1,20 +1,26 @@
 def solution(numbers, target):
     answer = 0
+    # 각 number마다 부호를 추가해서 2차원 행렬로 만듦.
     graph = [[] for _ in range(len(numbers))]
     for i in range(len(numbers)):
         graph[i].append(numbers[i])
         graph[i].append(numbers[i]*-1)
+    
+    # 계산된 값을 저장하기 위한 리스트 초기화
     queue = []
-    queue.append([graph[0][0], graph[0][1]])
+    queue.append(graph[0][0])
+    queue.append(graph[0][1])
+    
+    # 모든 경우의 수 탐색
     for i in range(1,len(numbers)): 
-        number = queue.pop(0)
         temp = []
-        for num in number: 
+        for num in queue: 
             for z in range(2):
                 temp.append(num+graph[i][z])
-        queue.append(temp)
+        queue= temp
     
-    for i in queue[0]:
+    # 모든 계산된 값들 중 target과 일치하는 값의 개수를 return
+    for i in queue:
         if i == target:
             answer +=1
     return answer
