9a66e0ced4c1b2b2a8431cd56532e3ef4f35e426
Baguette-bbang <dudals9701@naver.com>
2024-01-28 01:13:36 +0900

    complementation

--- BaekJoon/20000/20291.py
+++ BaekJoon/20000/20291.py
@@ -1,19 +1,7 @@
-# 파일을 확장자 별로 정리해서 몇 개씩 있는지 알려줘
-# 보기 편하게 확장자들을 사전 순으로 정렬해 줘
-
-dict = {}
-# 파일이 뭔지는 무시해도 됨.
-# 그저 확장자만이 중요함.
-# 즉, .이후의 단어에 대해 
-n = int(input())
+n = int(input()) # 파일의 수 
+extension_count = {} # 각 파일에서 확장자가 뭔지 구하고 각 확장자의 개수를 구해야함.
 for i in range(n):
-    file = input()
-    idx = file.find('.')
-    extension= file[idx+1:]
-    if extension not in dict:
-        dict[extension] = 1 
-    else:
-        dict[extension] += 1
-extensions = sorted(dict)
-for extension in extensions:
-    print(extension, dict[extension])
+    extension = input().split(".")[-1] # 파일은 파일명과 확장자로 구성되어 있으며 구분하는 것은 "."에 의해서임.
+    extension_count[extension] = extension_count.get(extension,0) + 1 # extension에 해당하는 값이 있다면 해당 값을 가져온 후 +1 없다면 0을 반환하여 +1
+for extension in sorted(extension_count):
+    print(extension, extension_count[extension])
