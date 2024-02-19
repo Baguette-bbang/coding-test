0c39519d426a5fe28afadf0189bbc84a2ab0d69d
Baguette-bbang <dudals9701@naver.com>
2024-02-19 16:19:09 +0900

    DP

---
+++ Programmers/level_2/피보나치_수.py
@@ -0,0 +1,7 @@
+def solution(n):
+    answer = 0
+    fibo = [0] * (n+1)
+    fibo[1], fibo[2] = 1, 1
+    for i in range(3, n+1):
+        fibo[i] = (fibo[i-1] + fibo[i-2])%1234567
+    return fibo[n]
