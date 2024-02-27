5236068ce45f69aaf7c5538c0f555b733a30e143
Baguette-bbang <dudals9701@naver.com>
2024-02-27 21:50:50 +0900

    Brute force

---
+++ BaekJoon/2000/2422.py
@@ -0,0 +1,19 @@
+# 모든 아이스크림은 1부터 N까지 번호가 매겨져
+# 아이스크림을 3가지 선택
+# 하지만 기피해야하는 조합이 있다.
+import sys 
+input = sys.stdin.readline
+from itertools import combinations
+n, m = map(int, input().split())
+not_combis = [list(map(int, input().split())) for _ in range(m)]
+combis = list(combinations([i for i in range(1, n+1)], 3))
+length = len(list(combis))
+real_not_combi = set()
+for not_combi in not_combis:
+    temp = [i for i in range(1, n+1)]
+    temp.remove(not_combi[0])
+    temp.remove(not_combi[1])
+    for t in temp:
+        temp_real = tuple(sorted(not_combi+[t]))
+        real_not_combi.add(temp_real)
+print(length - len(real_not_combi))
