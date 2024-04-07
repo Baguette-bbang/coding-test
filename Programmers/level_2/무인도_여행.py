eb926cd4d76dc7a78b88619e86f2bdf5319df6c1
Baguette-bbang <dudals9701@naver.com>
2024-04-07 16:40:13 +0900

    BFS & string

--- Programmers/level_2/무인도_여행.py
+++ Programmers/level_2/무인도_여행.py
@@ -7,16 +7,20 @@ def solution(maps):
     # 1. split 처리
     # 2. X가 아닌 위치 파악
     # 3. BFS 돌면서 visited 업데이트 및 섬의 기간 구하기
+    
     r_length = len(maps)
     c_length = len(maps[0])
     island_locations = []
     
+    # 1. 한 문자열을 문자로 나누기
     for i in range(len(maps)):        
         maps[i] = list(maps[i])
         for j in range(len(maps[i])):
+            # 2. X아닌 것의 위치 파악
             if maps[i][j] != 'X':
                 island_locations.append([i,j])
     
+    # 3. BFS 처리하기
     visited = [[False] * c_length for _ in range(r_length)]
     moves = [(0,1),(1,0),(-1,0),(0,-1)]
     def bfs(start):
