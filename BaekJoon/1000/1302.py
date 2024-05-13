4699c501bf4fa0e70479347229982776510adb12
Baguette-bbang <dudals9701@naver.com>
2024-05-13 14:14:38 +0900

    heap

---
+++ BaekJoon/1000/1302.py
@@ -0,0 +1,14 @@
+# 가장 많이 팔린 제목을 출력하는 것
+# 힙 자료구조를 사용해야 함.
+from heapq import heapify, heappop, heappush
+from collections import defaultdict
+n = int(input())
+heap = []
+heap_dict = defaultdict(int)
+for _ in range(n):
+    heap_dict[input()] += 1
+# print(heap_dict)
+
+for item in heap_dict.items():
+    heappush(heap, (-item[1], item[0]))
+print(heappop(heap)[1])
