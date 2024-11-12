b0d5fd0b2a4f81b68f6a3568bff5aca6ba615e86
Baguette-bbang <dudals9701@naver.com>
2024-11-12 15:10:32 +0900

    java : 입출력 연습, Math

--- BaekJoon/1000/1000.java
+++ BaekJoon/1000/1000.java
@@ -1,12 +1,29 @@
-import java.util.Scanner;
-public class Main{
+// import java.util.Scanner;
+// public class Main{
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int A,B;
-		A=scan.nextInt();
-		B=scan.nextInt();
+// 	public static void main(String[] args) {
+// 		Scanner scan = new Scanner(System.in);
+// 		int A,B;
+// 		A=scan.nextInt();
+// 		B=scan.nextInt();
 		
-		System.out.println(A+B);
+// 		System.out.println(A+B);
+// 	}
+// }
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		br.close();
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(String.valueOf(a + b));
+		bw.flush();
+		bw.close();
 	}
 }
